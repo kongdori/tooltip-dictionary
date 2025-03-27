05382b4dc077426d4b8b989d0bd1481c9f996197
Youngmin Koo <youngminz.kr@gmail.com>
2025-03-28 01:20:46 +0900

    feat: 확장 프로그램 아이콘 수정

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,5 +6,12 @@ export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
     permissions: ['storage'],
+    icons: {
+      16: 'icon/active16.png',
+      32: 'icon/active32.png',
+      48: 'icon/active48.png',
+      96: 'icon/active96.png',
+      128: 'icon/active128.png',
+    }
   },
 });
