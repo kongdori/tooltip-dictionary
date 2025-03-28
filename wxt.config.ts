914194ea529521feefeed91ff637f176fdb0d9ff
Youngmin Koo <youngminz.kr@gmail.com>
2025-03-28 23:52:59 +0900

    chore: 버전 업데이트 및 설정 추가

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
   manifest: {
+    name: 'Tooltip Dictionary',
     permissions: ['storage'],
     icons: {
       16: 'icon/active16.png',
@@ -12,6 +13,7 @@ export default defineConfig({
       48: 'icon/active48.png',
       96: 'icon/active96.png',
       128: 'icon/active128.png',
-    }
+    },
+    key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtxee2F/x9Qm2heCGgRXf2LqXpyMHYPYlpXx9HeFRHxvQBSN9ppmHJumACzLVTE9oVjYpASysk24GOA7e2vAX9BpNziUzIHHTfOELs0fUpNscHbuxJzFYzTg/PvFEm2yCkfcv9K/ebSgJ/EnSOa0Q9dM3NdoneZY2W1g8TlMdGszqoJOB9thba7bdF3YO9hysUswwAaO9dPrwKZPu8G4394ZFe3EXUXGIdXXomO3sb7c0/cjg7oysnSF63xNsZ45vKwaTM4rXYmQ4c+KRxNuBtPEZEQ4lQ3h+Az0qQu406KED8gjuYAyBpSm5N9fNmAfcGxiTT/ir8UsOA/3rpmDuZwIDAQAB'
   },
 });
